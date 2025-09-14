e013e3e7e0d48f31efab6f4bc6a04f5831ebbfa3
DSebas09 <sebas250902@hotmail.com>
2025-09-14 17:49:45 -0600

    feat: add basic Tetris game logic

--- scripts/boardTetris.js
+++ scripts/boardTetris.js
@@ -4,4 +4,49 @@ export class BoardTetris extends Grid {
     constructor(canvas, rows, cols, cellSize, space) {
         super(canvas, rows, cols, cellSize, space);
     }
+
+    isInside(row, col){
+        return row > 0 && row < this.rows && col > 0 && col < this.cols;
+    }
+
+    isEmpty(row, col){
+        return this.matriz[row][col] === 0 && this.isInside(row, col);
+    }
+
+    isRowFull(row){
+        return this.matriz[row].every(row => row !== 0);
+    }
+
+    isRowEmpty(row){
+        return this.matriz[row].every(row => row === 0);
+    }
+
+    clearRow(row){
+        this.matriz[row].fill(0);
+    }
+
+    moveRowDown(row, numRows){
+        this.matriz[row + numRows] = this.matriz[row].slice();
+        this.clearRow(row);
+    }
+
+    clearFullRows(){
+        let count = 0;
+
+        for(let row = this.rows - 1; row >= 0; row-- ) {
+            if (this.isRowFull(row)){
+                this.clearRow(row);
+                count++;
+            }
+            else if (count > 0) {
+                this.moveRowDown(row, count);
+            }
+        }
+
+        return count;
+    }
+
+    gameOver(){
+        return !(this.isRowEmpty(0));
+    }
 }
